10372dacadf5924e0ba9207604cbcb29f263cb10
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2024-12-21 12:17:07 +0700

    Implement database entities - Adds the following entity tables to the database: user, post, category, status, and voting.

---
+++ src/modules/statuses/entities/status.entity.ts
@@ -0,0 +1,23 @@
+import { Post } from 'src/modules/feedback-posts/entities/post.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('statuses')
+export class Status {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  name: string;
+
+  @OneToMany('Post', 'status')
+  posts: Post[];
+
+  @CreateDateColumn()
+  created_at: string;
+}
